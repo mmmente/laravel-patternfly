d1d0e8f41aec99a001d2357f5ff400ce020265af
Steven Crothers <steven.crothers@gmail.com>
2017-11-18 14:37:03 -0500

    Initial scaffolding for the Patternfly package.

---
+++ src/Facade.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Layerworx\Patternfly;
+
+class ServiceProvider extends \Illuminate\Support\Facades\Facade
+{
+  /**
+   * {@inheritDoc}
+   */
+  protected static function getFacadeAccessor()
+  {
+    return null;
+  }
+}
